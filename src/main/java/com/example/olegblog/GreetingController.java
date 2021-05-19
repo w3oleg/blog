b73d58473bad3682488a80d60506379a0d4d8a01
w3oleg <w3oleg>
2021-05-19 13:23:47 +0300

    add  project1

--- src/main/java/com/example/olegblog/GreetingController.java
+++ src/main/java/com/example/olegblog/GreetingController.java
@@ -9,7 +9,7 @@ import java.util.Map;
 
 @Controller
 public class GreetingController {
-
+//
     @GetMapping("/greeting")
     public String greeting(
             @RequestParam(name="name", required=false, defaultValue="World") String name,
